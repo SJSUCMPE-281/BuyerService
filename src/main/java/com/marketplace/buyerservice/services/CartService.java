25e07e3ff1d060a0956bdf46111154c46d6b26b4
Jeena <jeenathampi.23@gmail.com>
2020-11-19 03:57:08 -0800

    wired checkout flow

--- src/main/java/com/marketplace/buyerservice/services/CartService.java
+++ src/main/java/com/marketplace/buyerservice/services/CartService.java
@@ -8,8 +8,8 @@ import com.marketplace.buyerservice.repositories.CartRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.math.BigDecimal;
 import java.util.*;
-import java.util.UUID;
 
 @Service
 public class CartService {
@@ -18,21 +18,34 @@ public class CartService {
 
     public void save(Cart cart) throws JsonProcessingException {
         List<OrderDetails> orderDetailsList = cart.getOrderDetails();
-        Map<Product, Integer> map = new HashMap<>();
+        Map<String, Integer> map = new HashMap<>();
+        Map<String, Product> productMap = new HashMap<>();
         for(OrderDetails orderDetails: orderDetailsList){
-            if(map.containsKey(orderDetails.getProduct())){
-                map.put(orderDetails.getProduct(),map.get(orderDetails.getProduct())+1);
+            if(map.containsKey(orderDetails.getProduct().getProductId())){
+                map.put(orderDetails.getProduct().getProductId(),map.get(orderDetails.getProduct().getProductId())+1);
             }else{
-                map.put(orderDetails.getProduct(),orderDetails.getQuantity());
+                map.put(orderDetails.getProduct().getProductId(),orderDetails.getQuantity());
             }
+            productMap.put(orderDetails.getProduct().getProductId(),orderDetails.getProduct());
         }
         List<OrderDetails> newOrderDetailsList = new ArrayList<>();
-        Set<OrderDetails> set = new HashSet<>();
-        for(OrderDetails orderDetails: orderDetailsList) {
-            orderDetails.setQuantity(map.get(orderDetails.getProduct()));
-            set.add(orderDetails);
+        //Set<OrderDetails> set = new HashSet<>();
+        BigDecimal totalPrice = BigDecimal.ZERO;
+        BigDecimal taxAmount = BigDecimal.ZERO;
+        BigDecimal totalAmount = BigDecimal.ZERO;
+        for(String key: map.keySet()){
+            OrderDetails newOrderDetails = new OrderDetails();
+            newOrderDetails.setQuantity(map.get(key));
+            newOrderDetails.setProduct(productMap.get(key));
+            newOrderDetails.setOrderDetailAmount(newOrderDetails.getProduct().getPrice());
+            newOrderDetailsList.add(newOrderDetails);
+            totalPrice = totalPrice.add(newOrderDetails.getProduct().getPrice().multiply(new BigDecimal(newOrderDetails.getQuantity())));
         }
-        newOrderDetailsList.addAll(set);
+        cart.setTotalPrice(totalPrice);
+        taxAmount = totalPrice.multiply(new BigDecimal(5)).divide(new BigDecimal(100));
+        totalAmount = taxAmount.add(cart.getTotalPrice());
+        cart.setTaxAmount(taxAmount);
+        cart.setTotalAmount(totalAmount);
         cart.setOrderDetails(newOrderDetailsList);
         cart.setCartId(UUID.randomUUID().toString());
         cartRepository.add(cart);
